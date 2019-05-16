397fa835cbaed531ab0929445bf91fe9eefcc883
Khasim-AlRayan <khasim2880@gmail.com>
2019-05-16 18:36:57 +0530

    jwt auth changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,35 @@
 import React, {  
   Component,
 } from 'react';
+import axios from 'axios';
 
 export class Home extends Component {
+  constructor(props){
+    super(props);
+    this.state = {users: []};
+  }
+
+  componentDidMount(){
+    this.getAllUsers();
+  }
+
+  getAllUsers = () => {
+    axios.get("http://localhost:1330/users").then((res) => {
+      console.log('home data-->'+res.data);
+      this.setState({users: res.data});
+    }).catch((err) => {
+      this.props.history.push('login');
+    });
+  }
+
   render() {
-    return (
+    return this.state.users.length > 0 && (
       <div>
-        Welcome to home...!
+        {
+          this.state.users.map((obj, key)=>{
+            return <p key={key}>{obj.username}</p>;
+          })
+        }  
       </div>
     );
   }
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,16 +2,30 @@ import React, {
   Component,
 } from 'react';
 import '.././css/Login.css';
+import axios from 'axios';
 
 export class Login extends Component {
   constructor(props){
     super(props);
-    this.state = {};
+    this.state = {username: "", password: "", error: ""};
     this.Login = this.Login.bind(this);
   }
 
-  Login(){
-    this.props.history.push('home');
+  Login = () => {
+    axios.post("http://localhost:1330/authenticate", {"username":this.state.username, "password": this.state.password}).then((res, error) => {
+    console.log(error+'-'+res);  
+    if(!error){
+        this.props.history.push('home');
+      }else{
+        this.setState({error: 'Invalid user...!'});
+      }
+    }).catch((err) => {
+      this.setState({error: 'Invalid user...!'});
+    });
+  }
+
+  handleChange = (e, field) => {
+    this.setState({[field]: e.target.value});
   }
 
   render() {
@@ -24,15 +38,20 @@ export class Login extends Component {
             </tr>
             <tr>
               <td>Username</td>
-              <td><input type="text" /></td>
+              <td><input type="text" value={this.state.username} onChange={(e) => {this.handleChange(e, "username")}} /></td>
             </tr>
             <tr>
               <td>Password</td>
-              <td><input type="password" /></td>
+              <td><input type="password" value={this.state.password} onChange={(e) => {this.handleChange(e, "password")}} /></td>
             </tr>
             <tr>
               <td colSpan="2" className="centerAlign">
                 <button type="submit" onClick={this.Login}>Submit</button>
+              </td>
+            </tr>
+            <tr>
+              <td colSpan="2" className="centerAlign">
+                <label>{this.state.error}</label>
               </td>
             </tr>
           </tbody>
