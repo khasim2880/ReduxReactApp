18590464bed474f3fadb761a91c5ebb2666a705e
Khasim-AlRayan <khasim2880@gmail.com>
2019-05-17 11:47:06 +0530

    auth changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -31,7 +31,7 @@ export class Home extends Component {
           })
         }  
       </div>
-    );
+    )
   }
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -57,7 +57,7 @@ export class Login extends Component {
           </tbody>
         </table>
       </div>
-    );
+    )
   }
 }
 
